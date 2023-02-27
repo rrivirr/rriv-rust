a4ae5ec65ee741b669768420c4a29251d6f19cf5
Zaven Arra <zaven@waterbearsondes.io>
2023-02-27 11:17:34 -0800

    WIP remove-libs

---
+++ lib/remove-libs.sh
@@ -0,0 +1,9 @@
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.11.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.22.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.32.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.59.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.124.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.172.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.184.rcgu.o
+ar d librust_serial.a compiler_builtins-2ea1e06b41c95948.compiler_builtins.6278433c-cgu.206.rcgu.o
+
