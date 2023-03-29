1168ccb7eafff529e4ad53ffd2c573a056c5072f
m. interrupt <misterinterrupt@gmail.com>
2023-03-29 16:15:34 -0700

    re-set the pinmode so the led continues to work

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -13,6 +13,7 @@ void setup(void) {
   digitalWrite(PA5, HIGH);
   delay(500);
   rustSerial = rust_serial_interface_new();
+  pinMode(PA5, OUTPUT);
   digitalWrite(PA5, LOW);
   delay(250);
   digitalWrite(PA5, HIGH);
@@ -31,9 +32,7 @@ void loop(void)
   digitalWrite(PA5, HIGH);
   delay(500);
   uint8_t aChar = rust_serial_read(rustSerial);
-  rust_serial_write(rustSerial, aChar);
-  
-  
+  rust_serial_write(rustSerial, aChar);  
   digitalWrite(PA5, HIGH);
   delay(500);
   digitalWrite(PA5, LOW);
