ef78c59b2459645ba64f85abedc7b929614245d2
m. interrupt <misterinterrupt@gmail.com>
2023-08-16 16:47:02 -0700

    rm -rf /rust dir in order to merge this branch in good conscience ...

--- rust/src/processor/target/thumbv7m-none-eabi/debug/deps/processor-2f621923002a7051.d
+++
@@ -1,5 +0,0 @@
-/home/zaven/software-development/Water/rriv-rust/rust/src/processor/target/thumbv7m-none-eabi/debug/deps/processor-2f621923002a7051.rmeta: src/lib.rs
-
-/home/zaven/software-development/Water/rriv-rust/rust/src/processor/target/thumbv7m-none-eabi/debug/deps/processor-2f621923002a7051.d: src/lib.rs
-
-src/lib.rs:
--- rust/src/processor/target/x86_64-unknown-linux-gnu/debug/deps/processor-ef756cf43e23def8.d
+++
@@ -1,5 +0,0 @@
-/home/zaven/software-development/Water/rriv-rust/rust/src/processor/target/x86_64-unknown-linux-gnu/debug/deps/libprocessor-ef756cf43e23def8.a: src/lib.rs
-
-/home/zaven/software-development/Water/rriv-rust/rust/src/processor/target/x86_64-unknown-linux-gnu/debug/deps/processor-ef756cf43e23def8.d: src/lib.rs
-
-src/lib.rs:
