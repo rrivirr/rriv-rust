851fee67ba01940d603ef09df410107927fbf7a4
m. interrupt <misterinterrupt@gmail.com>
2023-08-23 21:13:58 -0700

    run rriv's lib init before maple's init - see description
    depends on https://github.com/rrivirr/maple-core-legacy/pull/1

--- test/cpp-runner/src/main.cpp
+++ test/cpp-runner/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "rrivrust.h"
 
-void * rriv_cmd;
+void *rriv_cmd;
 
 #define LED_PIN PA10
 
@@ -17,19 +17,13 @@ void datalogger_set(const char *json_strang)
   delay(75);
 }
 
-void setup(void) {
-  pinMode(LED_PIN, OUTPUT_OPEN_DRAIN);
-  digitalWrite(LED_PIN, LOW);
-  delay(750);
-  digitalWrite(LED_PIN, HIGH);
-  delay(750);
-  digitalWrite(LED_PIN, LOW);
-  delay(750);
-  digitalWrite(LED_PIN, HIGH);
-  delay(750);
-  digitalWrite(LED_PIN, LOW);
-  delay(750);
+void setup(void)
+{
+  // commented out the maple core's hidden, forced init() call
+  // so the rust hal can set clocks before the maple core does it for real
   rriv_cmd = command_service_init();
+  init();
+
   pinMode(LED_PIN, OUTPUT_OPEN_DRAIN);
   digitalWrite(LED_PIN, HIGH);
   delay(500);
