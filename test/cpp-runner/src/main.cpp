992a87de9c3b10da1bc5bbc60ec5607a2245aade
m. interrupt <misterinterrupt@gmail.com>
2023-09-10 18:57:10 -0700

    no tx.listen - C w/ rm-wholesale legacy maple branch

--- test/cpp-runner/src/main.cpp
+++ test/cpp-runner/src/main.cpp
@@ -1,4 +1,4 @@
-// #include <Arduino.h>
+#include <Arduino.h>
 #include "rrivrust.h"
 
 void *rriv_cmd;
@@ -22,8 +22,8 @@ void setup(void)
 
   // commented out the maple core's hidden, forced init() call
   // so the rust hal can set clocks before the maple core does it for real
+  init();
   rriv_cmd = command_service_init();
-  // init();
 
   // pinMode(LED_PIN, OUTPUT_OPEN_DRAIN);
   // digitalWrite(LED_PIN, HIGH);
