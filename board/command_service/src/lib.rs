77aabba7c16f83059361d918314c0bf82c80efb3
Zaven Arra <zaven@waterbearsondes.io>
2023-09-27 14:58:52 -0700

    wip: clean up comments

--- board/command_service/src/lib.rs
+++ board/command_service/src/lib.rs
@@ -36,11 +36,6 @@ struct CLICommand<'a> {
 impl CommandService {
     pub fn new() -> Self {
         // set the static, shareable command data
-        // cortex_m::interrupt::free(|cs| {
-        //     unsafe {
-        //     *COMMAND_DATA.borrow(cs).borrow_mut() = Some(CommandData::default());
-        //     }
-        // });
         CommandService {
             registry: CommandRegistry::new(),
         }
@@ -48,31 +43,12 @@ impl CommandService {
 
     /// set the global rx processor
     pub fn setup(&mut self, board: &mut impl RRIVBoard) {
-        // return if already setup
-        // this is not used, and unnecessary
-        // double calls to setup should be made to fail due to moves
-        // if self.check_setup() {
-        //     return;
-        // }
-        // create a new CharacterProcessor on the heap and leak it to a static lifetime
-        
-        // let char_processor = Box::<CharacterProcessor>::leak(Box::new(
-        //     CharacterProcessor::new(),
-        // ));
-
         let char_processor = Box::<CharacterProcessor>::leak(Box::new(
             CharacterProcessor::new(),
         ));
 
         // pass a pointer to the lleaked processor to Board::set_rx_processor
         board.set_rx_processor(Box::new(char_processor));
-
-        // set the setup flag to true
-        // I don't think this is the right idea.
-        // Also, why would we have multiple calls to setup that could race?  Do we need a setup flag?
-        // cortex_m::interrupt::free(|cs| {
-        //     SETUP_DONE.borrow(cs).replace(true);
-        // });
     }
 
     /// register a command with two &strs, object and action, and a C function pointer that matches registry.register_command's second argument
@@ -87,35 +63,11 @@ impl CommandService {
         self.registry.register_command(command, ffi_cb);
     }
 
-    // this is not used, and unnecessary.
-    // fn check_setup(&self) -> bool {
-    //     cortex_m::interrupt::free(|cs| {
-    //         if !*SETUP_DONE.borrow(cs).borrow() {
-    //             return false;
-    //         } else {
-    //             return true;
-    //         }
-    //     })
-    // }
-
-    /// get access to the static, shareable command data
-    // fn get_command_data(&self) -> &'static Mutex<RefCell<Option<CommandData>>> {
-    //     &COMMAND_DATA
-    // }
-
     fn pending_message_count(&self) -> usize {
-        // move critical section to board as a call with a closure
         // use critical section, don't need the Mutex and instead use unsafe{}
         cortex_m::interrupt::free(|cs| unsafe {
             let command_data = COMMAND_DATA.borrow_mut();
             CommandRecognizer::pending_message_count(&command_data)
-
-            // // let command_data = self.get_command_data().borrow(cs).borrow();
-            // if let Some(command_data) = command_data.deref() {
-            //     CommandRecognizer::pending_message_count(&command_data)
-            // } else {
-            //     0
-            // }
         })
     }
 
@@ -124,13 +76,6 @@ impl CommandService {
             unsafe {
                 let command_data = COMMAND_DATA.borrow_mut();
                 Ok(CommandRecognizer::take_command(command_data))
-
-                // let mut command_data = self.get_command_data().borrow(cs).borrow_mut();
-                // if let Some(command_data) = command_data.deref_mut() {
-                //     Ok(CommandRecognizer::take_command(command_data))
-                // } else {
-                //     Err(())
-                // }
             }
         })
     }
@@ -150,8 +95,6 @@ impl CommandService {
         rprintln!("{}", command_data_str);
 
         // Next
-        // 1. parse all the commands and validate them
-        //   a. investigate if we can use serde POJO style in no_std somehow
         // 2. port the EEPROM
         // 3. add and remove configurations from EEPROM
 
@@ -164,32 +107,6 @@ impl CommandService {
             }
             Err(_) => self.execute_command(Command::Unknown, command_data_cstr),
         }
-        // match serde_json::from_str::<Value>(command_data_str) {
-        //     Ok(data_json) => {
-        //         // Extract the command and object strings from the JSON
-        //         if !data_json.is_object() {
-        //             // handle this error
-        //             self.execute_command(Command::Unknown, command_data_cstr);
-        //             return;
-        //         }
-        //         if !data_json["object"].is_string() {
-        //             // handle this error
-        //             self.execute_command(Command::Unknown, command_data_cstr);
-        //             return;
-        //         }
-        //         if !data_json["action"].is_string() {
-        //             // handle this error
-        //             self.execute_command(Command::Unknown, command_data_cstr);
-        //             return;
-        //         }
-        //         let object_str = data_json["object"].as_str().unwrap();
-        //         let action_str = data_json["action"].as_str().unwrap();
-        //         // join the command and object strings with an underscore
-        //         let command = self.registry.get_command_from_parts(object_str, action_str);
-        //         self.execute_command(command, command_data_cstr);
-        //     }
-        //     Err(_) => self.execute_command(Command::Unknown, command_data_cstr),
-        // }
     }
 
     fn execute_command(&self, command: Command, command_cstr: &CStr) {
@@ -205,13 +122,10 @@ impl CommandService {
 }
 
 /// I believe the lifetimes here are generic over the lifetime of the CommandData
-pub struct CharacterProcessor {
-    // command_data: &'a CommandData,
-}
+pub struct CharacterProcessor { }
 
 impl<'a> CharacterProcessor {
     pub fn new() -> CharacterProcessor {
-        // CharacterProcessor { command_data }
         CharacterProcessor {  }
     }
 }
@@ -222,11 +136,6 @@ impl<'a, 'b> RXProcessor for CharacterProcessor {
             unsafe {
                 let command_data = COMMAND_DATA.borrow_mut();
                 CommandRecognizer::process_character(command_data, character);
-
-                // let mut command_data = self.command_data.borrow(cs).borrow_mut();
-                // if let Some(command_data) = command_data.deref_mut() {
-                //     CommandRecognizer::process_character(command_data, character);
-                // }
             }
         })
     }
