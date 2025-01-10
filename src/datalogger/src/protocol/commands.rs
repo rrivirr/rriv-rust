019dad2e7632dc5ccc6d1f7731b571e46856e42b
Zaven Arra <zaven.arra@gmail.com>
2025-01-09 17:03:15 -0800

    wip: organize code and return values

---
+++ src/datalogger/src/protocol/commands.rs
@@ -0,0 +1,41 @@
+use alloc::format;
+
+use rriv_board::RRIVBoard;
+use rtt_target::rprintln;
+
+use crate::BoardGetPayload;
+
+// use super::board_response::BoardResponse;
+
+
+pub fn get_board(board: &mut impl RRIVBoard, payload: BoardGetPayload){
+
+    // let mut board_response = BoardResponse::new();
+    
+    if let Some(param) = payload.parameter {
+        match param {
+            serde_json::Value::String(param) => {
+                rprintln!("{:?}", param.as_str());
+                match param.as_str() {
+                    "epoch" => {
+                        let epoch = board.epoch_timestamp();
+                        board.serial_send(format!("{:}\n", epoch).as_str());
+                    }
+                    _ => {
+                        board.serial_send("Unsupported param in command\n");
+                    }
+                }
+            }
+            err => {
+                board.serial_send("Bad param in command\n");
+                rprintln!("Bad epoch {:?}", err);
+                return;
+            }
+
+        }
+    } else {
+        let epoch = board.epoch_timestamp();
+        board.serial_send(format!("{:}", epoch).as_str());                
+    }
+
+}
