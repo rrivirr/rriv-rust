33c78bb3c08275b0d6f6e22ba8fbaa3db3e40aa8
jake <jhosen@purdue.edu>
2025-01-19 18:28:31 -0500

    first go

--- src/datalogger/src/drivers/ds18b20.rs
+++ src/datalogger/src/drivers/ds18b20.rs
@@ -1,6 +1,7 @@
 use super::types::*;
 
 pub const EMPTY_SIZE: usize = 32;
+pub const NUMBER_OF_MEASURED_PARAMETERS: usize = 2;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Ds18b20SpecialConfiguration {
@@ -26,7 +27,21 @@ impl Ds18b20SpecialConfiguration {
 
 pub struct Ds18b20 {
     general_config: SensorDriverGeneralConfiguration,
-    special_config: Ds18b20SpecialConfiguration
+    special_config: Ds18b20SpecialConfiguration,
+    measured_parameter_values: [f64; NUMBER_OF_MEASURED_PARAMETERS]
+}
+
+impl Ds18b20 {
+    pub fn new (
+        general_config: SensorDriverGeneralConfiguration,
+        special_config: Ds18b20SpecialConfiguration
+    ) -> Ds18b20 {
+        Ds18b20 {
+            general_config,
+            special_config,
+            measured_parameter_values: [0.0, NUMBER_OF_MEASURED_PARAMETERS]
+        }
+    }
 }
 
 impl SensorDriver for Ds18b20 {
@@ -37,18 +52,32 @@ impl SensorDriver for Ds18b20 {
     getters!();
 
     fn get_measured_parameter_count(&mut self) -> usize {
-        todo!()
+        return NUMBER_OF_MEASURED_PARAMETERS;
     }
 
     fn get_measured_parameter_value(&mut self, index: usize) -> f64 {
-        todo!()
+        if index < NUMBER_OF_MEASURED_PARAMETERS {
+            return Ok(sel.measured_parameter_values[index]);
+        } else {
+            return Err(());
+        }
     }
 
     fn get_measured_parameter_identifier(&mut self, index: usize) -> [u8;16] {
-        todo!()
+        let identifiers: [&str; NUMBER_OF_MEASURED_PARAMETERS] = ["T_raw","T_cal"];
+        let mut buf: [u8; 16] = [0_u8; 16];
+
+        let mut identifier: &str = "invalid";
+        if index <= NUMBER_OF_MEASURED_PARAMETERS {
+            identifier = identifiers[index];
+        }
+        for i: usize in 0..identifier.len() {
+            buf[i] = identifier.as_bytes()[i];
+        }
+        return buf;
     }
 
     fn take_measurement(&mut self, board: &mut dyn rriv_board::SensorDriverServices) {
-        todo!()
+        ds18b20::
     }
 }
