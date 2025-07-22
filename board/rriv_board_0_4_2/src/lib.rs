a1a55cb7fbd242205536b031278f6a8435f067b7
Zaven Arra <zaven.arra@gmail.com>
2025-07-22 14:03:18 -0700

    feat: turn power to intADC ports off and then on during startup, this can be used to power cycle external 3v3 components

--- board/rriv_board_0_4_2/src/lib.rs
+++ board/rriv_board_0_4_2/src/lib.rs
@@ -1099,6 +1099,8 @@ impl BoardBuilder {
         let internal_adc_configuration =
             InternalAdcConfiguration::new(internal_adc_pins, device_peripherals.ADC1);
         let mut internal_adc = internal_adc_configuration.build(&clocks);
+        internal_adc.disable();
+        delay.delay_ms(1000_u32);
         internal_adc.enable(&mut delay);
         self.internal_adc = Some(internal_adc);
 
